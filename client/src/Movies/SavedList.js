5f2834566a3dd58f46075533a7ac1dfc75994c1a
Liam Sutton <devliam90@gmail.com>
2019-09-16 19:00:56 +0100

    add navlink and active styles  to saved movies

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const SavedList = props => (
   <div className='saved-list'>
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <Link to={`/movies/${movie.id}`} key={movie.id}>
+      <NavLink
+        to={`/movies/${movie.id}`}
+        key={movie.id}
+        activeStyle={{
+          fontWeight: 'bold',
+          color: 'red'
+        }}
+      >
         <span className='saved-movie'>{movie.title}</span>
-      </Link>
+      </NavLink>
     ))}
     <Link to='/'>
       <div className='home-button'>Home</div>
